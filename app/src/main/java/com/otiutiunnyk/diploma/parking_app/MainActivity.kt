d60d1d3d567659714d4fcb3d532f958ed9772c79
alex-tiutiunnyk <tytynnik6377@gmail.com>
2023-05-02 17:20:16 +0300

    init commit

---
+++ app/src/main/java/com/otiutiunnyk/diploma/parking_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.otiutiunnyk.diploma.parking_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.otiutiunnyk.diploma.parking_app.ui.theme.ParkingappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParkingappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ParkingappTheme {
+        Greeting("Android")
+    }
+}
