7dd206ba976b85b6f0647c38f674d44177daba0a
alex-tiutiunnyk <tytynnik6377@gmail.com>
2023-05-07 00:22:24 +0300

    Add map to the AddNewRequest page

--- app/src/main/java/com/otiutiunnyk/diploma/parking_app/ui/screen/bottomNav/AddNewPlaceScreen.kt
+++ app/src/main/java/com/otiutiunnyk/diploma/parking_app/ui/screen/bottomNav/AddNewPlaceScreen.kt
@@ -1,9 +1,54 @@
 package com.otiutiunnyk.diploma.parking_app.ui.screen.bottomNav
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
 
 @Composable
 fun AddNewPlaceScreen() {
-    Text(text = "Add New Place Screen") //TODO: decide on how it will look like
+    NewGoogleMaps()
+}
+
+@Composable
+fun NewGoogleMaps() {
+    val properties by remember {
+        mutableStateOf(
+            MapProperties(
+                isMyLocationEnabled = true,
+                isTrafficEnabled = true,
+                isBuildingEnabled = true,
+                mapType = MapType.NORMAL
+            )
+        )
+    }
+    val uiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(
+                myLocationButtonEnabled = true,
+                zoomControlsEnabled = false
+            )
+        )
+    }
+    val kyiv = LatLng(50.44, 30.52)
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        properties = properties,
+        uiSettings = uiSettings,
+        cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(kyiv, 12f)
+        }
+    ) {
+        Marker(
+            state = rememberMarkerState(position = kyiv), //change to the center always (see the video)
+            icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+        )
+    }
 }
